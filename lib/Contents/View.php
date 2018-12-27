5b983d00adde6a8adf01575a131f80a952a29ea8
Michael J Rubinsky <mrubinsk@horde.org>
2018-12-27 10:10:59 -0500

    Escape the '-', this is not a range.

--- lib/Contents/View.php
+++ lib/Contents/View.php
@@ -48,7 +48,7 @@ class IMP_Contents_View
         global $page_output, $session;
 
         $headers = $this->_contents->getHeader();
-        $zipfile = trim(preg_replace('/[^\pL\pN-+_. ]/u', '_', $headers->getValue('subject')), ' _');
+        $zipfile = trim(preg_replace('/[^\pL\pN\-+_. ]/u', '_', $headers->getValue('subject')), ' _');
         if (empty($zipfile)) {
             $zipfile = _("attachments.zip");
         } else {
@@ -197,7 +197,7 @@ class IMP_Contents_View
         $session->close();
 
         $name = ($subject = $this->_contents->getHeader()->getValue('subject'))
-            ? trim(preg_replace('/[^\pL\pN-+_. ]/u', '_', $subject), ' _')
+            ? trim(preg_replace('/[^\pL\pN\-+_. ]/u', '_', $subject), ' _')
             : 'saved_message';
 
         return array(
