2b5cf3b5db2c6e4468b8b6a1a689adb8d17694f0
Ralf Lang <lang@b1-systems.de>
2021-02-24 14:39:14 +0000

    Development mode for imp-7.0.0alpha3

--- lib/Application.php
+++ lib/Application.php
@@ -64,7 +64,7 @@ class IMP_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (7.0.0alpha1)';
+    public $version = 'H5 (7.0.0alpha2)';
 
     /**
      * Server key used in logged out session.
