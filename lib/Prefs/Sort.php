c15d7f7eef208dc194b919709dcb3a5af861ae04
Ralf Lang <ralf.lang@ralf-lang.de>
2022-10-12 17:59:40 +0200

    Address ZDI-20-1051 / ZDI-CAN-10436: Prevent deserializing a class.
    Also guard against some other possibly unwanted deserialisations.
    It is debatable if this constitutes an actual attack vector before the change.
    However, the change rules out any such possibility.

--- lib/Prefs/Sort.php
+++ lib/Prefs/Sort.php
@@ -39,9 +39,13 @@ class IMP_Prefs_Sort implements ArrayAccess, IteratorAggregate
     {
         global $prefs;
 
-        $sortpref = @unserialize($prefs->getValue(self::SORTPREF));
-        if (is_array($sortpref)) {
-            $this->_sortpref = $sortpref;
+        $serializedPref = $prefs->getValue(self::SORTPREF);
+        // Only unserialize non-empty strings. Disallow yielding any classes.
+        if (!empty($serializedPref && is_string($serializedPref))) {
+            $sortpref = @unserialize($serializedPref, ['allowed_classes' => false]);
+            if (is_array($sortpref)) {
+                $this->_sortpref = $sortpref;
+            }
         }
     }
 
