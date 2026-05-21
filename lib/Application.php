0ffa2fdea7a911cbcbe75c62a2f979b76106baa9
Ralf Lang <ralf.lang@ralf-lang.de>
2026-05-21 14:10:50 +0200

    chore(release): bump version to 7.0.0-RC2
    Release version 7.0.0-RC2
    
    Class IMP_Imap not found
    feat(imap): add XOAUTH2 authentication support
    Implement try-catch for old server key retrieval
    style: PER-1
    refactor: Identity Factory now returns the PSR-4 class
    refactor: Degrate IMP_Prefs_Identity to a shim for Horde\Imp\IdentityDriverPrefs
    refactor(Identity): Build a PSR-4 Identity Driver
    fix: Initialize stdClass before accessing attributes

--- lib/Application.php
+++ lib/Application.php
@@ -67,7 +67,7 @@ class IMP_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = '7.0.0-RC1';
+    public $version = '7.0.0-RC2';
 
     /**
      * Server key used in logged out session.
